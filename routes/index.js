7b47f3f900b20d8cf02ecc6d64620016b8b5afd7
developerjin <devjin0617@gmail.com>
2018-04-19 23:48:34 +0900

    Add thumbnail

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var s3 = require('../lib/s3')
 var moment = require('moment')
 var hash = require('object-hash')
+var imageLib = require('../lib/image')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -28,15 +29,25 @@ router.post('/s3/file', async function(req, res) {
   var filename = hash.MD5(file.md5 + (new Date().getTime())) + '.' + ext
   var path = moment().format('YYYY/MM/DD/')
 
+  console.log('file upload...')
   var result = await s3.upload({
     path: path + filename,
     file: file.data
   })
+  console.log('file upload success!')
 
+  imageLib.createThumbnailWithS3Upload({
+    file: file.data,
+    name: filename,
+    path: path + filename,
+  })
+
+  var url = `https://s3.ap-northeast-2.amazonaws.com/${s3.bucketName}/${path}${filename}`
   res.send({
     success: true,
     data: {
-      url: `https://s3.ap-northeast-2.amazonaws.com/${s3.bucketName}/${path}${filename}`
+      url: url,
+      thumbnail: url + '.thumb'
     }
   })
 
