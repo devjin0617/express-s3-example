9866295437d1a56d70744f8a5879f872e8e74623
developerjin <devjin0617@gmail.com>
2018-04-19 23:25:55 +0900

    Modify filename and path

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var s3 = require('../lib/s3')
-
+var moment = require('moment')
+var hash = require('object-hash')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -22,17 +23,20 @@ router.post('/s3/file', async function(req, res) {
 
   var file = files[keys[0]]
 
+  var ext = file.name.split('.').slice(-1)[0]
+
+  var filename = hash.MD5(file.md5 + (new Date().getTime())) + '.' + ext
+  var path = moment().format('YYYY/MM/DD/')
+
   var result = await s3.upload({
-    path: file.name,
+    path: path + filename,
     file: file.data
   })
-
-  console.log(result)
 
   res.send({
     success: true,
     data: {
-      url: `https://s3.ap-northeast-2.amazonaws.com/${s3.bucketName}/${file.name}`
+      url: `https://s3.ap-northeast-2.amazonaws.com/${s3.bucketName}/${path}${filename}`
     }
   })
 
